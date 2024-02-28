065672364510e8cc5186e935a752ed338a33c6be
gathem <anoneg655@gmail.com>
2024-02-28 21:01:34 +0200

    Updated

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { request } from "express";
 import bcrypt from "bcrypt";
 
 const app = express();
-const port = 3000;
+const port = 2459;
 
 const users = [];
 app.use(express.json());
@@ -26,21 +26,24 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const findUser = users.find((data) => email == data.email);
+    const findUser = users.find((data) => email === data.email);
     if (!findUser) {
-      res.status(400).send("wrong email or password!");
+      res.status(400).send("wrong email");
+      return
     }
     const passwordMatch = await bcrypt.compare(password, findUser.password);
     if (passwordMatch) {
       res.status(200).send("Logged in successfully!");
     } else {
-      res.status(400).send("wrong email or password!");
+      res.status(400).send("wrong password!");
+      return
     }
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
+  console.log(req.body)
 });
 
 app.listen(port, () => {
-  console.log("server started on port 3000");
+  console.log(`server started on port ${port}`);
 });
